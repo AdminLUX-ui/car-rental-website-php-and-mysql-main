42cf254b48bd8f774e378794db33a30f82f34c2a
AdminLUX-ui <shantanukulkarni@luxcarrentalcomau.com>
2025-05-15 16:47:26 +1000

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,19 +1,17 @@
 <?php
-    $dsn = 'mysql:host=lux-mysql-server.mysql.database.azure.com;dbname=car_rental';
-        $user = 'mysqladmin@lux-mysql-server';
-        $pass = 'Admin@123';
-        $option = array(
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-        );
-        try
-        {
-                $con = new PDO($dsn,$user,$pass,$option);
-                $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-                //echo 'Good Very Good !';
-        }
-        catch(PDOException $ex)
-        {
-                echo "Failed to connect with database ! ".$ex->getMessage();
-                die();
-        }
+$dsn = 'mysql:host=lux-mysql-server.mysql.database.azure.com;dbname=car_rental;sslmode=require';
+$user = 'mysqladmin@lux-mysql-server';
+$pass = 'Admin@123';
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+);
+
+try {
+    $con = new PDO($dsn, $user, $pass, $options);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $ex) {
+    echo "Failed to connect with database! " . $ex->getMessage();
+    die();
+}
 ?>
