8f806e27519fa5af251b0b2464af557760abe794
AdminLUX-ui <shantanukulkarni@luxcarrentalcomau.com>
2025-05-15 20:56:45 +1000

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ $pass = 'Admin@123';
 $options = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
 );
-echo "Connected successfully";
+
 try {
     $con = new PDO($dsn, $user, $pass, $options);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
